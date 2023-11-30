29a9ae7be668c5a69d183cd70737b69fbb702b35
aman190 <ajaman190@kgpian.iitkgp.ac.in>
2023-11-30 23:35:25 +0530

    apnainsti contact us page
    Signed-off-by: aman190 <ajaman190@kgpian.iitkgp.ac.in>

---
+++ Backend/src/controllers/index.js
@@ -0,0 +1,75 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+async function SayHello(req, res) {
+  try {
+    // testing connection
+    res.status(200).json({
+      success: true,
+      message: "Hello, ✅ Backend connected successfully!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: error,
+    });
+  }
+}
+
+async function ContactUs(req, res) {
+  try {
+    const { name, mobile, email, message } = req.body;
+
+    // create reusable transport object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.sendinblue.com",
+      port: 587,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    // setuping email data with html body
+    const mailOptions = {
+      from: `${name} <${email}>`, // sender address
+      to: "apnainsti@kgpian.iitkgp.ac.in", // receiver address
+      subject: "New Query", // subject line
+      html: `<div style="margin: 0; margin-bottom: 16px; background-color: #f0f0f0; border-radius: 10px; padding: 16px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);">
+      <p style="margin: 0; margin-bottom: 16px;"><i>User Contact Us Query?</i></p>
+      <p style="margin: 0; margin-bottom: 4px;">Name: ${name}</p>
+      <p style="margin: 0; margin-bottom: 4px;">Mobile: ${mobile}</p>
+      <p style="margin: 0; margin-bottom: 4px;">Email: ${email}</p>
+      <p style="margin: 0; ">Message: ${message}</p>
+      </div>
+      <p style="margin: 0;">Thank you. Looking forward to hear positive response from your team soon.</p>`,
+    };
+
+    // sending mail with defined transport object
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log("Error: " + error);
+        res.status(400).json({
+          success: false,
+          message: "📪 Failed to send your message!",
+        });
+      } else {
+        console.log("Email sent: " + info.response);
+        res.status(200).json({
+          success: true,
+          message: "📫 Your message has been sent successfully!",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: error,
+    });
+  }
+}
+
+module.exports = {
+  SayHello,
+  ContactUs,
+};
---
+++ README.md
@@ -0,0 +1,33 @@
+## Contact Us Page
+
+This is a contact us page of ApnaInsti app which takes user's query and send the data to apnainsti@kgpian.iitkgp.ac.in
+
+<img src="https://github.com/ajaman190/ContactUs/blob/master/Media/Screenshot.png"  width="350">
+
+## Installation
+
+### Backend
+
+Download the "Backend" folder and open it in vscode.
+
+```bash
+  npm install
+  npm run dev
+```
+
+### Frontend
+
+Download the "Frontend" folder and open it in vscode. Next, connect your Android device to your computer using a USB cable and ensure that USB debugging is enabled on your device. Alternatively, you can use an emulator instead of an Android device. Finally, run the commands
+
+```bash
+  npm install
+  npm run android
+```
+
+Now to establish communication between the backend server running on localhost on your desktop and the app running on your Android device, run the below command in the terminal after building the app.
+
+```bash
+  adb reverse tcp:4000 tcp:4000
+```
+
+This 'adb reverse' command is used to set up reverse port forwarding between a device and a computer. In this case, it forwards TCP traffic from port 4000 on the computer to port 4000 on the device.
